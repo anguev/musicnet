82bb905be3f32c615b9941d62cc183f51ecd91e5
anguev <131567462+anguev@users.noreply.github.com>
2025-12-29 19:42:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
   fetch("https://musicnet-backend.onrender.com/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      email: document.getElementById("email").value,
-      password: document.getElementById("password").value
-    })
+    body: JSON.stringify({ email, password })
   })
   .then(res => {
     if (!res.ok) throw new Error("Login incorrecto");
     return res.json();
   })
-  .then(data => alert("Bienvenido " + data.name))
-  .catch(() => alert("Error de login"));
+  .then(data => {
+    alert("Bienvenido " + data.name);
+  })
+  .catch(() => {
+    alert("Credenciales incorrectas");
+  });
 }
-
